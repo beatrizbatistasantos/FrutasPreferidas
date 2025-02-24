28e117c1e00d0f79a71c705bc8ff147f9580f1ab
beatrizbatista <byah24@outlook.com>
2025-02-24 11:13:53 -0300

    Finaliza programa Frutas Preferidas

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("///////////////////////////");
+Console.WriteLine("//////Frutas Favoritas/////");
+Console.WriteLine("///////////////////////////");
+
+Console.WriteLine();
+
+Console.Write("1. ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.WriteLine("Morango <A melhor de todas>");
+Console.ResetColor();
+
+Console.Write("2. ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("Uva Thompson");
+Console.ResetColor();
+
+Console.Write("3. ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("Banana");
+Console.ResetColor();
+
+Console.Write("4. ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("Pera");
+Console.ResetColor();
+
+Console.Write("5. ");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.WriteLine("Pitaya");
+Console.ResetColor();
